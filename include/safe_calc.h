432974ddca29987912d6712d7c6163efc26d70e5
mtueih <mtueih.org@outlook.com>
2026-05-06 12:06:59 +0800

    2026-05-06 12:06:59

--- include/safe_calc.h
+++ include/safe_calc.h
@@ -6,6 +6,19 @@
 	#include <stdbool.h>
 #endif
 
-bool safe_size_add(size_t a, size_t b, size_t *result);
+#include <attrs.h>
+
+/**
+ * @brief 安全地执行 size_t 加法，检测溢出
+ * @param a 第一个操作数
+ * @param b 第二个操作数
+ * @param result 输出结果（不能为 NULL）
+ * @return true 如果没有溢出，false 如果溢出
+ */
+bool safe_size_add(
+	size_t a,
+	size_t b,
+	size_t *result
+) ATTRS_PURE ATTRS_NONNULL(3);
 
 #endif
