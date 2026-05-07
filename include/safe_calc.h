6128e0d055f7927ed2676605c409d3880b4c882c
mtueih <mtueih.org@outlook.com>
2026-05-07 20:57:04 +0800

    2026-05-07 20:57:04

--- include/safe_calc.h
+++ include/safe_calc.h
@@ -21,4 +21,63 @@ bool safe_size_add(
 	size_t *result
 ) ATTRS_PURE ATTRS_NONNULL(3);
 
+/**
+ * @brief 测试 size_t 加法是否会溢出
+ * @param a 第一个操作数
+ * @param b 第二个操作数
+ * @return true 会溢出，不会溢出
+ */
+bool safe_size_add_test(
+	size_t a,
+	size_t b
+) ATTRS_PURE;
+
+/**
+ * @brief 安全地执行 size_t 减法，检测溢出
+ * @param a 第一个操作数
+ * @param b 第二个操作数
+ * @param result 输出结果（不能为 NULL）
+ * @return true 如果没有溢出，false 如果溢出
+ */
+bool safe_size_sub(
+	size_t a,
+	size_t b,
+	size_t *result
+) ATTRS_PURE ATTRS_NONNULL(3);
+
+/**
+ * @brief 测试 size_t 减法是否会溢出
+ * @param a 第一个操作数
+ * @param b 第二个操作数
+ * @return true 会溢出，不会溢出
+ */
+bool safe_size_sub_test(
+	size_t a,
+	size_t b
+) ATTRS_PURE;
+
+/**
+ * @brief 安全地执行 size_t 向上对齐到某个数的整数倍
+ * @param x 要对齐的数
+ * @param align 对齐值
+ * @param result 输出结果（不能为 NULL）
+ * @return true 如果没有溢出，false 如果溢出
+ */
+bool safe_size_align_up(
+	size_t x,
+	size_t align,
+	size_t *result
+) ATTRS_PURE ATTRS_NONNULL(3);
+
+/**
+ * @brief 测试 size_t 向上对齐到某个数的整数倍是否会溢出
+ * @param x
+ * @param align
+ * @return true 如果没有溢出，false 如果溢出
+ */
+bool safe_size_align_up_test(
+	size_t x,
+	size_t align
+) ATTRS_PURE;
+
 #endif
--- src/main.c
+++ src/main.c
@@ -19,6 +19,26 @@ int main(void) {
 		printf("size_t 加法（%zu + %zu）正常！result：%zu\n", a, b, result);
 	}
 
+	// 减法测试
+	a = 0;
+	b = 1;
+
+	if (!safe_size_sub(a, b, &result)) {
+		printf("size_t 减法（%zu - %zu）下溢！result：%zu（不变）\n", a, b, result);
+	} else {
+		printf("size_t 减法（%zu - %zu）正常！result：%zu\n", a, b, result);
+	}
+
+	// 对齐测试
+	b = ((size_t)-1 >> 2) * 3;
+	a = b + 1;
+
+	if (!safe_size_align_up(a, b, &result)) {
+		printf("size_t 对齐（%zu ⬆ %zu）上溢！result：%zu（不变）\n", a, b, result);
+	} else {
+		printf("size_t 对齐（%zu ⬆ %zu）正常！result：%zu\n", a, b, result);
+	}
+
 
 	return EXIT_SUCCESS;
 }
